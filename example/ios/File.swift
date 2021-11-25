a10698d197d042577c2c08d0cbdbaf0b19789d50
ohohohjkl <anhtuck01@gmail.com>
2021-11-25 14:32:43 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  AwesomeTestingExample
+//
+
+import Foundation
---
+++ ios/AwesomeTesting.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface AwesomeTesting : NSObject <RCTBridgeModule>
+
+@end
